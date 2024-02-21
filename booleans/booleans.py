cf40cc44e1bf7d54e2aedaceb9c068c76e61218c
Oleksandr Karadiaur <karadiaur.oleksandr@gmail.com>
2024-02-21 18:57:52 +0100

    committed lesson for booleans

---
+++ booleans/booleans.py
@@ -0,0 +1,29 @@
+print(10 > 9)  # true
+print(10 == 9)  # false
+print(10 < 9)  # false
+
+# region start True
+
+print(bool("Hello"))
+print(bool(15))
+
+x = "Hello"
+y = 15
+
+print(bool(x))
+print(bool(y))
+
+# end
+
+# region start False
+
+bool(False)
+bool(None)
+bool(0)
+bool("")
+bool(())
+bool([])
+bool({})
+
+# end
+
